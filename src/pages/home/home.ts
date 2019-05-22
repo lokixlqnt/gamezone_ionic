58918b2255044bf1acf9132e8cc53bf64f10430f
Thomas Tondeur <thomasinbac@gmail.com>
2019-05-22 12:04:41 +0200

    buttons + alerts

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController, Button } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +7,73 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public alertCtrl: AlertController) {
 
   }
-
+  showLogin() {
+  const prompt = this.alertCtrl.create({
+    title: 'Login',
+    message: "Entrez vos informations de connexion",
+    inputs: [
+      {
+        name: 'pseudo',
+        placeholder: 'Pseudo'
+      },
+      {
+        name: 'pass',
+        placeholder: 'Mot de passe'
+      },
+    ],
+    buttons: [
+      {
+        text: 'Cancel',
+        handler: data => {
+          console.log('Cancel clicked');
+        }
+      },
+      {
+        text: 'Login',
+        handler: data => {
+          console.log('Saved clicked');
+        }
+      }
+    ]
+  });
+  prompt.present();
+}
+showSignup() {
+const prompt = this.alertCtrl.create({
+  title: 'Signup',
+  message: "Entrez vos informations de connexion",
+  inputs: [
+    {
+      name: 'pseudo',
+      placeholder: 'Pseudo'
+    },
+    {
+      name: 'pass',
+      placeholder: 'Mot de passe'
+    },
+    {
+      name: 'mail',
+      placeholder: 'E-mail'
+    }
+  ],
+  buttons: [
+    {
+      text: 'Cancel',
+      handler: data => {
+        console.log('Cancel clicked');
+      }
+    },
+    {
+      text: 'Login',
+      handler: data => {
+        console.log('Saved clicked');
+      }
+    }
+  ]
+});
+prompt.present();
+}
 }
