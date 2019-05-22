80d125de8ab3630f508a91660b37b4067f1f8d4a
Thomas Tondeur <thomasinbac@gmail.com>
2019-05-22 15:00:58 +0200

    firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,14 @@ import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
 
 import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
+import { FIREBASE_CONFIG } from '../firebase.credentials';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -17,7 +20,9 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     AboutPage,
     ContactPage,
     HomePage,
-    TabsPage
+    TabsPage,
+    AngularFireModule.initializeApp(FIREBASE_CONFIG)
+    AngularFireDatabaseModule
   ],
   imports: [
     BrowserModule,
---
+++ src/app/firebase.credentials.ts
@@ -0,0 +1,9 @@
+export const FIREBASE_CONFIG = {
+    apiKey: "AIzaSyABkOU8cpF67IFDJmSKAm4LHUANVrKEmvY",
+    authDomain: "gamezone-fa93d.firebaseapp.com",
+    databaseURL: "https://gamezone-fa93d.firebaseio.com",
+    projectId: "gamezone-fa93d",
+    storageBucket: "gamezone-fa93d.appspot.com",
+    messagingSenderId: "1037648408876",
+    appId: "1:1037648408876:web:22f3f5f101699d1a"
+  };
